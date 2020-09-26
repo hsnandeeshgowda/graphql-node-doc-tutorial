b38bf41fb1ecc47f76544d000391165c81c48dfe
hsnandeeshgowda <hsnandeeshgowda@gmail.com>
2020-09-26 23:56:12 +0530

    basic type definations and resolvers

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga');
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
 
-// 2
+let links = [
+    {
+        id:1,
+        description:"testdesc",
+        url:"test.com"
+    },
+]
+
+let idCount = links.length
+
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
+    Query:{
+        info:()=>`This is the API of a Hackernews Clone`,
+        feed: () => links,
+    },
+    Link : {
+        id : (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
+    },
+    Mutation:{
+        post : (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+              }
+              links.push(link)
+              return link
+        },
+    }
 }
 
-// 3
-const server = new GraphQLServer({
-  typeDefs,
-  resolvers,
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+const server = new GraphQLServer({ typeDefs:'./src/schema.graphql', resolvers });
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
